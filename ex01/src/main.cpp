c35dc3e5604dd77e33b3979d346d1f9cdc8a5d77
LuisTakeo <luistakeo@hotmail.com>
2024-07-30 18:52:00 -0300

    Feat: criado loop para leitura de input e tratamento para NULL no cin

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -11,10 +11,27 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdio>
 #include <cstdlib>
+#include <string>
 
 int	main(void)
 {
+	std::string	line;
+
 	std::cout << "Welcome to Nakama Book!" << std::endl;
+	while (1)
+	{
+		std::clearerr(stdin);
+		std::cin.clear();
+		std::cout << "Insert an option: ", std::getline(std::cin, line);
+		// if (std::cin.eof())
+		// 	std::cout << std::endl;
+		if (line == "EXIT")
+			break;
+		std::cout << line << std::endl;
+
+	}
+	std::cout << "Bye!" << std::endl;
 	return (EXIT_SUCCESS);
 }
